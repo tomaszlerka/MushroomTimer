5ce277ce12db03874c8642a908cd8eb8b95a05b3
Tomasz Lerka <tomaszlerka98@gmail.com>
2020-07-22 19:06:57 +0200

    Implement small changes to data for 215 Rise Surge of Power

--- Data/215-RiseSurgeOfPower.js
+++ Data/215-RiseSurgeOfPower.js
@@ -8,42 +8,10 @@ let event = [
         countdownEnd: ''
     },
     {
-        name: 'Yum Yum Island',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#yum',
-        date: new Date("2020/08/13 02:00:00"),
-        dateEnd: '-',
-        countdown: '',
-        countdownEnd: ''
-    },
-    {
-        name: 'Burning World',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#burning',
+        name: 'Eileen\'s Support',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#support',
         date: new Date("2020/07/23 01:00:00"),
-        dateEnd: new Date("2020/09/16 23:00:00"),
-        countdown: '',
-        countdownEnd: ''
-    },
-    {
-        name: 'World Leap',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#leap',
-        date: new Date("2020/09/17 00:00:00"),
-        dateEnd: new Date("2020/10/06 23:00:00"),
-        countdown: '',
-        countdownEnd: ''
-    },
-    {
-        name: 'Relay',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#relay',
-        date: new Date("2020/07/23 01:00:00"),
-        dateEnd: new Date("2020/08/26 02:00:00"),
-        countdown: '',
-        countdownEnd: ''
-    },
-    {
-        name: 'Rise Shops',
-        link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#stamp',
-        date: new Date("2020/06/11 01:00:00"),
-        dateEnd: new Date("2020/08/31 02:00:00"),
+        dateEnd: new Date("2020/08/05 02:00:00"),
         countdown: '',
         countdownEnd: ''
     },
@@ -56,14 +24,6 @@ let event = [
         countdownEnd: ''
     },
     {
-        name: 'Rise Fountain #3',
-        link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#fountain',
-        date: new Date("2020/08/05 02:00:00"),
-        dateEnd: new Date("2020/08/26 02:00:00"),
-        countdown: '',
-        countdownEnd: ''
-    },
-    {
         name: 'Rise Coin Shop #2',
         link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#coin',
         date: new Date("2020/07/09 02:00:00"),
@@ -72,18 +32,26 @@ let event = [
         countdownEnd: ''
     },
     {
-        name: 'Rise Coin Shop #3',
-        link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#coin',
-        date: new Date("2020/08/05 02:00:00"),
-        dateEnd: new Date("2020/08/31 02:00:00"),
+        name: 'Relay',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#relay',
+        date: new Date("2020/07/23 01:00:00"),
+        dateEnd: new Date("2020/08/26 02:00:00"),
         countdown: '',
         countdownEnd: ''
     },
     {
-        name: 'Eileen\'s Support',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#support',
-        date: new Date("2020/07/23 01:00:00"),
-        dateEnd: new Date("2020/08/05 02:00:00"),
+        name: 'Moonbogglers',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#moon',
+        date: new Date("2020/08/05 02:00:00"),
+        dateEnd: new Date("2020/08/12 02:00:00"),
+        countdown: '',
+        countdownEnd: ''
+    },
+    {
+        name: 'Rise Coin Express',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#express',
+        date: new Date("2020/08/05 02:00:00"),
+        dateEnd: new Date("2020/08/19 02:00:00"),
         countdown: '',
         countdownEnd: ''
     },
@@ -96,10 +64,26 @@ let event = [
         countdownEnd: ''
     },
     {
-        name: 'Rise Coin Express',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#express',
+        name: 'Rise Fountain #3',
+        link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#fountain',
         date: new Date("2020/08/05 02:00:00"),
-        dateEnd: new Date("2020/08/19 02:00:00"),
+        dateEnd: new Date("2020/08/26 02:00:00"),
+        countdown: '',
+        countdownEnd: ''
+    },
+    {
+        name: 'Rise Coin Shop #3',
+        link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#coin',
+        date: new Date("2020/08/05 02:00:00"),
+        dateEnd: new Date("2020/08/31 02:00:00"),
+        countdown: '',
+        countdownEnd: ''
+    },
+    {
+        name: 'Rise Shops',
+        link: 'http://maplestory.nexon.net/news/58015/v-214-rise-promise-of-the-guardian-patch-notes#stamp',
+        date: new Date("2020/06/11 01:00:00"),
+        dateEnd: new Date("2020/08/31 02:00:00"),
         countdown: '',
         countdownEnd: ''
     },
@@ -136,6 +120,14 @@ let event = [
         countdownEnd: ''
     },
     {
+        name: 'Yum Yum Island',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#yum',
+        date: new Date("2020/08/13 02:00:00"),
+        dateEnd: '-',
+        countdown: '',
+        countdownEnd: ''
+    },
+    {
         name: 'Combo God: Gloves',
         link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#combo',
         date: new Date("2020/08/12 02:00:00"),
@@ -152,10 +144,18 @@ let event = [
         countdownEnd: ''
     },
     {
-        name: 'Moonbogglers',
-        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#moon',
-        date: new Date("2020/08/05 02:00:00"),
-        dateEnd: new Date("2020/08/12 02:00:00"),
+        name: 'Burning World',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#burning',
+        date: new Date("2020/07/23 01:00:00"),
+        dateEnd: new Date("2020/09/16 23:00:00"),
+        countdown: '',
+        countdownEnd: ''
+    },
+    {
+        name: 'World Leap',
+        link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#leap',
+        date: new Date("2020/09/17 00:00:00"),
+        dateEnd: new Date("2020/10/06 23:00:00"),
         countdown: '',
         countdownEnd: ''
     },
