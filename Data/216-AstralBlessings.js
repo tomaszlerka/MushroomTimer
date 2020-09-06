366aaa0f299d0a0332914623ebd9fd3aaad42526
Tomasz Lerka <tomaszlerka98@gmail.com>
2020-09-06 17:14:23 +0200

    Update for patch 216-AstralBlessings

---
+++ Data/216-AstralBlessings.js
@@ -0,0 +1,42 @@
+let event = [
+  {
+      name: 'Celestial Festival',
+      link: 'https://maplestory.nexon.net/news/60439/updated-august-25-v-216-astral-blessings-patch-notes#celestial',
+      date: new Date("2020/08/26 01:00:00"),
+      dateEnd: new Date("2020/09/23 02:00:00"),
+      countdown: '',
+      countdownEnd: ''
+  },
+  {
+    name: 'Burning World',
+    link: 'http://maplestory.nexon.net/news/59255/v-215-rise-surge-of-power-patch-notes#burning',
+    date: new Date("2020/07/23 01:00:00"),
+    dateEnd: new Date("2020/09/16 23:00:00"),
+    countdown: '',
+    countdownEnd: ''
+},
+{
+    name: 'World Leap',
+    link: 'https://maplestory.nexon.net/news/60439/updated-august-25-v-216-astral-blessings-patch-notes#leap',
+    date: new Date("2020/09/17 00:00:00"),
+    dateEnd: new Date("2020/10/06 23:00:00"),
+    countdown: '',
+    countdownEnd: ''
+},
+{
+    name: 'Fairy Bros\' Golden Giveaway',
+    link: 'https://maplestory.nexon.net/news/60439/updated-august-25-v-216-astral-blessings-patch-notes#giveaway',
+    date: new Date("2020/08/26 01:00:00"),
+    dateEnd: new Date("2020/11/17 02:00:00"),
+    countdown: '',
+    countdownEnd: ''
+},
+{
+    name: 'I haven\'t done anything',
+    link: 'https://maplestory.nexon.net/news/60439/updated-august-25-v-216-astral-blessings-patch-notes#anything',
+    date: new Date("2020/09/24 14:00:00"),
+    dateEnd: new Date("2020/10/07 02:00:00"),
+    countdown: '',
+    countdownEnd: ''
+},
+];
