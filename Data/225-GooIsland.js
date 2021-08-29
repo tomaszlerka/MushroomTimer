e78ce5ea406ae783da8a3d9993705766991cf58a
Tomasz Lerka <tomaszlerka98@gmail.com>
2021-08-29 20:08:36 +0200

    WIP - Update for v225

---
+++ Data/225-GooIsland.js
@@ -0,0 +1,312 @@
+let schema = [
+    {
+        category: "Category",
+        categoryColor: "",
+        sectionTitle: "Section Title",
+        sectionLink: "#",
+        sectionDescription: "Section Description",
+        item: [
+            {
+                itemTitle: "Item Title",
+                itemDescription: "Item Description",
+                itemLink: "",
+                itemS: new Date("Start date"),
+                itemE: new Date("End date"),
+            },
+        ],
+    },
+];
+
+let eventList = [
+    //Category: Main Events
+    {
+        category: "NEO",
+        categoryColor: "#80f0ff",
+        sectionTitle: "Neo Castle",
+        sectionLink:
+            "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#castle",
+        sectionDescription:
+            "Participate in the Neo Castle events to collect Neo Stones, Neo Gems, and Neo Cores!",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+        ],
+    },
+    {
+        category: "NEO",
+        categoryColor: "#80f0ff",
+        sectionTitle: "Neo Crystal Challenge",
+        sectionLink:
+            "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#challenge",
+        sectionDescription:
+            "Destroy 12 stages of mobs and bosses over 2 minutes to get up to 1500 Crystal Points per day, which can be used to buy Growth Potions.",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+        ],
+    },
+    {
+        category: "NEO",
+        categoryColor: "#80f0ff",
+        sectionTitle: "Leo's Travel Journal",
+        sectionLink:
+            "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#journal",
+        sectionDescription: "Weekly challenges for Neo Stones.",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+        ],
+    },
+    {
+        category: "NEO",
+        categoryColor: "#80f0ff",
+        sectionTitle: "Neo Stones Capping",
+        sectionLink: "",
+        sectionDescription: "",
+        item: [
+            {
+                itemTitle: "Renee and the Magic Bell",
+                itemDescription: "",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#bell",
+                itemS: new Date("2021/07/22 00:00 UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+        ],
+    },
+    {
+        category: "NEO",
+        categoryColor: "#80f0ff",
+        sectionTitle: "Neo Gems Minigames",
+        sectionLink: "",
+        sectionDescription: "",
+        item: [
+            {
+                itemTitle: "Supernatural Yut",
+                itemDescription: "",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#yut",
+                itemS: new Date("2021/07/22 00:00 UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Rainbow Rush",
+                itemDescription: "",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#rush",
+                itemS: new Date("2021/06/10 12:00 AM UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Castle Column",
+                itemDescription: "",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#column",
+                itemS: new Date("2021/06/30 12:00 AM UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Thinking of You",
+                itemDescription: "",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#think",
+                itemS: new Date("2021/07/22 00:00 UTC"),
+                itemE: new Date("2021/08/24 11:59 PM UTC"),
+            },
+        ],
+    },
+    {
+        category: "NEO",
+        categoryColor: "#80f0ff",
+        sectionTitle: "Neo Shops",
+        sectionLink: "",
+        sectionDescription: "",
+        item: [
+            {
+                itemTitle: "Neo Stone Shop",
+                itemDescription: "You can earn Neo Stones by point capping.",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#stone",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/29 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Neo Gem Shop",
+                itemDescription: "You can earn Neo Gems by playing mini-games.",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#gem",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/29 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Neo Core Shop",
+                itemDescription:
+                    "You can earn Neo Cores by defeating weekly bosses.",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#core",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/29 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Neo Meso Shop",
+                itemDescription: "",
+                itemLink:
+                    "https://maplestory.nexon.net/news/67904/v-224-neo-light-s-wrath-patch-notes#meso",
+                itemS: new Date("2021/06/10 00:00 UTC"),
+                itemE: new Date("2021/08/29 11:59 PM UTC"),
+            },
+        ],
+    },
+
+    //Category: Burning
+    {
+        category: "Burning",
+        categoryColor: "#fc874c",
+        sectionTitle: "Burning World Leap",
+        sectionLink:
+            "https://maplestory.nexon.net/news/68486/updated-august-27-v-225-goo-island-getaway-patch-notes#leap",
+        sectionDescription:
+            "Move up to 6 characters from Burning world to other reg worlds.",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/08/25 12:00 AM UTC"),
+                itemE: new Date("2021/09/09 00:00 UTC"),
+            },
+        ],
+    },
+
+    //Category: Other Events
+    {
+        category: "Other Events",
+        categoryColor: "#ffc817",
+        sectionTitle: "Fairy Bros’ Golden Giveaway",
+        sectionLink:
+            "https://maplestory.nexon.net/news/68486/updated-august-27-v-225-goo-island-getaway-patch-notes#fairy",
+        sectionDescription:
+            "Stay logged in for an hour each day and check in with the Fairy Bros to get attendance rewards.",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/08/26 12:00 AM UTC"),
+                itemE: new Date("2021/11/16 11:59 PM UTC"),
+            },
+        ],
+    },
+
+    {
+        category: "Other Events",
+        categoryColor: "#ff75de",
+        sectionTitle: "Return of the Mobile Mansion",
+        sectionLink:
+            "https://maplestory.nexon.net/news/68486/updated-august-27-v-225-goo-island-getaway-patch-notes#mobile",
+        sectionDescription: "",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/09/15 12:00 AM UTC"),
+                itemE: new Date("2021/10/05 11:59 PM UTC"),
+            },
+        ],
+    },
+
+    {
+        category: "Other Events",
+        categoryColor: "#00ffb3",
+        sectionTitle: "Animal Friend Safari",
+        sectionLink:
+            "https://maplestory.nexon.net/news/68486/updated-august-27-v-225-goo-island-getaway-patch-notes#animal",
+        sectionDescription: "",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/09/22 12:00 AM UTC"),
+                itemE: new Date("2021/09/28 11:59 PM UTC"),
+            },
+        ],
+    },
+
+    {
+        category: "Other Events",
+        categoryColor: "#fff461",
+        sectionTitle: "Sunny Sunday",
+        sectionLink:
+            "https://maplestory.nexon.net/news/68486/updated-august-27-v-225-goo-island-getaway-patch-notes#sunny",
+        sectionDescription: "",
+        item: [
+            {
+                itemTitle: "Week 1 - August 29",
+                itemDescription:
+                    "100% extra EXP from runes, <br> 300% extra EXP from combo orbs",
+                itemS: new Date("2021/08/29 12:00 AM UTC"),
+                itemE: new Date("2021/08/29 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Week 2 - September 5",
+                itemDescription: "2x EXP from Pollo & Fritto and Inferno Wolf",
+                itemS: new Date("2021/09/05 12:00 AM UTC"),
+                itemE: new Date("2021/09/05 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Week 3 - September 12",
+                itemDescription:
+                    "Receive 3 Mysterious Monsterbloom, <br> 2x Chance for Monster Collection",
+                itemS: new Date("2021/09/12 12:00 AM UTC"),
+                itemE: new Date("2021/09/12 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Week 4 - September 19",
+                itemDescription: "5, 10, 15 Star Forcing",
+                itemS: new Date("2021/09/19 12:00 AM UTC"),
+                itemE: new Date("2021/09/19 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Week 5 - September 26",
+                itemDescription:
+                    "100% extra EXP from runes, <br> 300% extra EXP from combo orbs",
+                itemS: new Date("2021/09/26 12:00 AM UTC"),
+                itemE: new Date("2021/09/26 11:59 PM UTC"),
+            },
+            {
+                itemTitle: "Week 6 - October 3",
+                itemDescription: "2x EXP from Pollo & Fritto and Inferno Wolf",
+                itemS: new Date("2021/10/03 12:00 AM UTC"),
+                itemE: new Date("2021/10/03 11:59 PM UTC"),
+            },
+        ],
+    },
+
+    {
+        category: "Other Events",
+        categoryColor: "#5cd1ff",
+        sectionTitle: "Spell Trace Fever",
+        sectionLink:
+            "https://maplestory.nexon.net/news/68486/updated-august-27-v-225-goo-island-getaway-patch-notes#spell",
+        sectionDescription:
+            "Increased spell trace success rate during specified 2-hour windows.",
+        item: [
+            {
+                itemTitle: "",
+                itemDescription: "",
+                itemS: new Date("2021/09/24 08:00 AM UTC"),
+                itemE: new Date("2021/09/26 04:00 AM UTC"),
+            },
+        ],
+    },
+];
